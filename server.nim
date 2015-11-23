bc57b26fd33c7520624a2e7af707d6819d2c4a8f
Anton Ruban <a.ruban.documents@gmail.com>
2015-11-23 12:10:13 +0200

    Moved logic finding existed screen-files to server-file. Add variable to index proc, with exist screens. Improved  setImages proc. Reverse screen-files array data in view.

--- server.nim
+++ server.nim
@@ -1,16 +1,36 @@
 # Server file. Provides routes and working with URLs.
-import future, asyncdispatch, strutils ,sequtils, asyncdispatch
+import future, asyncdispatch, strutils ,sequtils, asyncdispatch, os
 import jester, templates
 import layouts
 
-proc getImages(request: PRequest): string =
+const
+  DirName: string = "savedscreens/"
+  format: string = "*.png"
+type
+  stringArray = seq[string]
+var
+  existScrens: stringArray
+
+existScrens = @[]
+
+for file in walkFiles(DirName & format):
+  existScrens.add(file)
+
+proc setImages(request: Request): string =
   var screenshots = request.formData["screenshots[]"]
-  let filesPath = "savedscreens/" & screenshots.fields["filename"]
-  writeFile(filesPath, screenshots.body)
+  let fileName = screenshots.fields["filename"]
+
+  try:
+    writeFile(DirName & fileName, screenshots.body)
+  except IOError:
+    let msg = getCurrentExceptionMsg()
+    echo "Screenshot $currentScreen not written to $folder " & msg
+
+  existScrens.add(DirName & fileName)
   return ""
 
 routes:
-  post "/": resp getImages(request)
-  get "/":    resp layouts.index(request)
+  post "/": resp setImages(request)
+  get "/":  resp layouts.index(request, existScrens)
 
 runForever()
--- views.nim
+++ views.nim
@@ -5,8 +5,12 @@ import templates
 proc allScreensView*(title, content: string): string = tmpli html"""
   <!DOCTYPE html>
   <link rel="stylesheet" href="/styles.css">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" integrity="sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ==" crossorigin="anonymous">
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js" integrity="sha512-K1qjQ+NcF2TYO/eI3M6v8EiNYZfA95pQumfvcVrTHtwQVDG+aHRqLi/ETn2uB+1JqwYqVG3LIvdm9lj6imS/pQ==" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css"
+        integrity="sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ=="
+        crossorigin="anonymous">
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"
+          integrity="sha512-K1qjQ+NcF2TYO/eI3M6v8EiNYZfA95pQumfvcVrTHtwQVDG+aHRqLi/ETn2uB+1JqwYqVG3LIvdm9lj6imS/pQ=="
+          crossorigin="anonymous"></script>
   <link href='https://fonts.googleapis.com/css?family=Play&subset=latin,cyrillic' rel='stylesheet' type='text/css'>
   <title>TS - $title</title>
   <div id=container>
