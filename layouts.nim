dd85aa3cc67dcf76337149bb3f1787bd541b5abe
antnruban <antnruban@gmail.com>
2015-11-29 23:48:22 +0200

    Fix index function

--- layouts.nim
+++ layouts.nim
@@ -1,14 +1,13 @@
 # File with page layouts.
-import templates, views, jester, os
+import templates, views, jester
 
 # All screens layout
-proc index*(request: Request): string =
-  let files_path = "savedscreens/*.png"
+proc index*(request: Request, existScrens: seq[string]): string =
   tmpli  html"""
     <div class="list-screens">
       <div class="row">
         <ul>
-          $for screen in walkFiles($files_path) {
+          $for screen in existScrens {
             <li><a href="$screen" class="thumbnail"><img src="$screen" alt="screen not available"></a></li>
           }
         </div>
