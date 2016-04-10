1022c7aa877718c89fcd8666f9f275fba0c91ca2
antnruban <antnruban@gmail.com>
2016-04-10 12:56:09 +0300

    Create File#create functionality.

--- app/controllers/api/v1/file_controller.rb
+++ app/controllers/api/v1/file_controller.rb
@@ -1,25 +1,36 @@
 class Api::V1::FileController < Api::V1::BaseController
   skip_before_filter :verify_authenticity_token
+  before_action :exist_dir, only: :create
 
   UTF8_ENCODING = Encoding::UTF_8
-  DATA_DIR = 'public/data'
-  
+  DATA_DIR = Rails.root.join('public','data')
+
   def create
-    Dir.mkdir(DATA_DIR) unless Dir.exist?(DATA_DIR)
+    params[:screenshots].each do |screen|
+      data = screen.read
+      if data.empty?
+        return api_error(status: 422, errors: 'bad data.')
+      else
+        path = Rails.root.join(DATA_DIR, screen.original_filename)
+        save_file(path, data)
 
-
-    params[:screenshots].each do |file|
-      path = File.join(DATA_DIR, file.original_filename)
-      File.open(path, "wb") do |data|
-
-        byebug
-
-        data.encode(UTF8_ENCODING) if data.read.encoding != UTF8_ENCODING
-        data.write
-
-        return api_error(status: 422, errors: user.errors) unless File.exist?(path)
+        return api_error(status: 422, errors: 'file not saved.') unless File.exist?(path)
         render json: { success: 'data saved' }, status: 200
       end
     end
+  end
+
+  private
+#######################################################################################################################
+
+  def save_file(path, data)
+    File.open(path, 'wb') do |file|
+      data.force_encoding(UTF8_ENCODING) if data.encoding != UTF8_ENCODING
+      file.write(data)
+    end
+  end
+
+  def exist_dir
+    Dir.mkdir(DATA_DIR) unless Dir.exist?(DATA_DIR)
   end
 end
